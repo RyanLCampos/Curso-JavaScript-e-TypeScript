eea696453e3599ae1f94de28e369ba9f2e8bd875
Ryan Lucas <ryanlcampos19@gmail.com>
2025-08-20 18:54:53 -0300

    :sparkles: Feat: adiciona aula sobre operadores logicos

---
+++ JS/JS_LOGICA_PROGRAMACAO/Aula16_OperadoresLogicos/index.js
@@ -0,0 +1,14 @@
+/*
+    Operadores Lógicos
+    && -> AND
+    || -> OU
+    ! -> NOT
+
+*/ 
+
+const usuario = 'Ryan';
+const senha = 12345;
+
+const vaiLogar = usuario === 'Ryan' && senha === 12345;
+
+console.log(vaiLogar);
