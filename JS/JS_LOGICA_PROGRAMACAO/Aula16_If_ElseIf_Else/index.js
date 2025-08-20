17e4978dbc1c0236c1103933138e602f9bffb35b
Ryan Lucas <ryanlcampos19@gmail.com>
2025-08-20 20:54:06 -0300

    :sparkles: Feat: adiciona aula sobre if, if-else, else

---
+++ JS/JS_LOGICA_PROGRAMACAO/Aula16_If_ElseIf_Else/index.js
@@ -0,0 +1,20 @@
+const hora = 50;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom Dia!');
+}else if (hora >= 12 && hora <= 17){
+    console.log('Boa Tarde!');
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa Noite');
+}else {
+    console.log('Olá!');
+}
+
+const tenhoDinheiro = NaN;
+
+if(tenhoDinheiro) {
+    console.log('Vou sair de casa');
+}else {
+    console.log('Não vou sair de casa');
+}
+
