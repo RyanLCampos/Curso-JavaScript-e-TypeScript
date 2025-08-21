85f73b4003f8e1156f6d3ecfe3873e483fe6bdb7
Ryan Lucas <ryanlcampos19@gmail.com>
2025-08-21 12:38:26 -0300

    :sparkles: Feat: adiciona aula sobre Switch/Case

---
+++ JS/JS_LOGICA_PROGRAMACAO/Aula19_SwitchCase/index.js
@@ -0,0 +1,38 @@
+const data = new Date();
+
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(`${diaSemanaTexto} (${diaSemana})`);
+
+function getDiaSemanaTexto(diaSemana) {
+
+  let diaSemanaTexto
+
+  switch (diaSemana) {
+    case 0:
+      diaSemanaTexto = 'Domingo';
+      return diaSemanaTexto;
+    case 1:
+      diaSemanaTexto = 'Segunda-feira';
+      return diaSemanaTexto;
+    case 2:
+      diaSemanaTexto = 'Terça-feira';
+      return diaSemanaTexto;
+    case 3:
+      diaSemanaTexto = 'Quarta-feira';
+      return diaSemanaTexto;
+    case 4:
+      diaSemanaTexto = 'Quinta-feira';
+      return diaSemanaTexto;
+    case 5:
+      diaSemanaTexto = 'Sexta-feira';
+      return diaSemanaTexto;
+    case 6:
+      diaSemanaTexto = 'Sabado';
+      return diaSemanaTexto;
+    default:
+      diaSemanaTexto = 'Valor inválido!';
+      return diaSemanaTexto;
+  }
+}
