19a9ce1225bba6029f0aa69ed21ea681c3a96c9d
Ryan Lucas <ryanlcampos19@gmail.com>
2025-08-26 13:20:42 -0300

    :sparkles: Feat: adiciona implementação Lista de Tarefas

---
+++ JS/JS_LOGICA_PROGRAMACAO/Aula30_Criando_Lista_Tarefas/assets/js/main.js
@@ -0,0 +1,109 @@
+const inputTarefa = document.querySelector(".input-tarefa");
+const listaTarefas = document.querySelector(".tarefas");
+
+
+
+
+inputTarefa.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    adicionaTarefa();
+  }
+});
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+
+    if(el.classList.contains('btn-remover')){
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+
+    if(el.classList.contains('btn-add-tarefa')){
+        adicionaTarefa();
+    }
+
+    if(el.classList.contains('btn-remover-todos')){
+        removerTodasTarefas();
+    }
+});
+
+function adicionaTarefa() {
+  if (!inputTarefa.value) return;
+
+  criaTarefa(inputTarefa.value);
+}
+
+function limpaInput() {
+    inputTarefa.value = '';
+}
+
+function criaTarefa(tarefa) {
+  const li = criaLi(tarefa);
+
+  listaTarefas.appendChild(li);
+  
+  criaButtonRemover(li);
+  limpaInput();
+  salvarTarefas();
+}
+
+function criaLi(texto) {
+  const li = document.createElement("li");
+  li.setAttribute('class', 'item-tarefa');
+
+  li.textContent = texto;
+
+  return li;
+}
+
+function criaButtonRemover(li) {
+  const btn = document.createElement("button");
+
+  btn.setAttribute('class', 'btn-remover');
+
+  btn.innerText = "Remover";
+
+  li.appendChild(btn);
+
+  return btn;
+}
+
+function salvarTarefas(){
+    const liTarefas = listaTarefas.querySelectorAll('li');
+    const tarefas = [];
+
+    for(let tarefa of liTarefas){
+        const tarefaTexto = tarefa.innerText;
+
+        let novaTarefaTexto = tarefaTexto.replace('Remover', '').trim();
+        tarefas.push(novaTarefaTexto);
+
+    }
+
+    const tarefasJSON = JSON.stringify(tarefas);
+
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function recuperaTarefasSalvas(){
+    let tarefasSalvas = localStorage.getItem('tarefas');
+    
+    tarefasSalvas = JSON.parse(tarefasSalvas);
+
+    for(let tarefa of tarefasSalvas){
+        criaTarefa(tarefa);
+    }
+}
+
+function removerTodasTarefas(){
+    if(confirm("Quer realmente apagar tudo?")){
+        while(listaTarefas.firstChild){
+            listaTarefas.removeChild(listaTarefas.firstChild);
+        }
+
+        salvarTarefas();
+    }
+}
+
+
+recuperaTarefasSalvas();
