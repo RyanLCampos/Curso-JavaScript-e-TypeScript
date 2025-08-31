ebf3dcbf5d4c95122b296a88cd5c71613dd50278
Ryan Lucas <ryanlcampos19@gmail.com>
2025-08-30 21:35:47 -0300

    :sparkles: Feat: adiciona aula sobre ForEach

---
+++ JS/MODULO_4_JS_ARRAYS_AVANCADO/Aula8_ForEach/index.js
@@ -0,0 +1,14 @@
+/*
+    array.forEach(function(valor, indice, array) {
+        Implementação codigo...
+    });
+*/
+
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let valor of a1){
+    console.log(valor);
+}
+
+a1.forEach(valor => console.log(valor));
