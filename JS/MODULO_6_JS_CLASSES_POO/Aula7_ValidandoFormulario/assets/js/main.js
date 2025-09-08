3eb2c9cc6b4f7d04f5b2306b3d893c23075c2faa
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-08 12:19:08 -0300

    :sparkles: Feat: adiciona aula sobre validação de formulario utilizando classe

---
+++ JS/MODULO_6_JS_CLASSES_POO/Aula7_ValidandoFormulario/assets/js/main.js
@@ -0,0 +1,111 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario');
+        this.eventos();
+    }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhasSaoValidas();
+
+        if(camposValidos && senhasValidas) {
+            alert('Formulário Enviado!');
+            this.formulario.submit();
+        }
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+            errorText.remove();
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText;
+
+            if(!campo.value) {
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
+                valid = false;
+            }
+
+            if(campo.classList.contains('cpf')) {
+                if(!this.validaCPF(campo)) valid = false;
+            }
+
+            if(campo.classList.contains('usuario')) {
+                if(!this.validaUsuario(campo)) valid = false;
+            }
+        }
+
+        return valid;
+    }
+
+    senhasSaoValidas() {
+        let valid = true; 
+
+        const senha  = this.formulario.querySelector('.senha');
+
+        const confirmaSenha = this.formulario.querySelector('.confirmar-senha');
+
+        if(senha.value !== confirmaSenha.value) {
+            valid = false; 
+            
+            this.criaErro(senha, 'Campos senha e confirmar senha precisam ser iguais.');
+            this.criaErro(confirmaSenha, 'Campos senha e confirmar senha precisam ser iguais.');
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12) {
+            valid = false;
+
+            this.criaErro(senha, 'Senha precisa ter entre 6 e 12 caracteres.');
+        }
+
+        return valid;
+    } 
+
+
+    validaCPF(campo) {
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()) {
+            this.criaErro(campo, 'CPF Inválido');
+            return false;
+        }
+
+        return true;
+    }
+
+    validaUsuario(campo) {
+        
+        const usuario = campo.value;
+
+        if(usuario.length < 3 || usuario.length > 12) {
+            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.');
+            return false;
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+            this.criaErro(campo, 'Usuário precisa conter apenas letras e/ou números.');
+        }
+
+        return true;
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
+    }
+}
+
+const validaFormulario = new ValidaFormulario();
