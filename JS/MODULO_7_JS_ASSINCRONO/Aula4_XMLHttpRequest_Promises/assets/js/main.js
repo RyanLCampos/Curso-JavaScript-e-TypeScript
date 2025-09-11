4a4b96101977f56fc8fde30e8087ecc60baaf19a
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-11 17:07:15 -0300

    :sparkles: Feat: aula sobre Fetch API (GET)

---
+++ JS/MODULO_7_JS_ASSINCRONO/Aula4_XMLHttpRequest_Promises/assets/js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+
+    try {
+        const href = el.getAttribute('href');
+    
+        const response = await fetch(href);
+    
+        const html = await response.text();
+    
+        carregaResultado(html);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+
+    resultado.innerHTML = response;
+}
