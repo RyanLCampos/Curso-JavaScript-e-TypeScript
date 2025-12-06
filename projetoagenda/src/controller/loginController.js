53c38497a637932c484be289b7f59e574e958e13
Ryan Lucas <ryanlcampos19@gmail.com>
2025-12-06 15:08:35 -0300

    :sparkles: Feat: adiciona aula (Logando o usuario)

--- projetoagenda/src/controller/loginController.js
+++ projetoagenda/src/controller/loginController.js
@@ -1,6 +1,7 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
+    if(req.session.user) return res.render('login-logado');
     res.render('login');
 };
 
@@ -27,3 +28,41 @@ exports.register = async function(req, res) {
         return res.render('404');
     }
 };
+
+exports.login = async function(req, res) {
+
+    try {
+        const login = new Login(req.body);
+        await login.entrar();
+
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function() { // Salva a sessão
+                return res.redirect('/login');     // Call back para a pagina anterior
+            });
+            return;
+        }
+
+        if(!login.user) {
+            req.flash('errors', login.errors);
+            req.session.save(function() { // Salva a sessão
+                return res.redirect('/login');     // Call back para a pagina anterior
+            });
+            return;
+        }
+
+        req.flash('success', 'Logado com sucesso!');
+        req.session.user = login.user;
+        req.session.save(function() { // Salva a sessão
+            return res.redirect('/login');     // Call back para a pagina home
+        });
+    }catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
