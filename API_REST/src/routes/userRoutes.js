a516b3671e6fd728bcf34616f5dd642c887f646a
Ryan Lucas <ryanlcampos19@gmail.com>
2025-12-11 13:59:15 -0300

    :sparkles: Feat: adiciona aula (CRUD de usuários)

--- API_REST/src/routes/userRoutes.js
+++ API_REST/src/routes/userRoutes.js
@@ -4,7 +4,11 @@ import userController from '../controllers/userController';
 
 const router = new Router();
 
+router.get('/', userController.index);
 router.post('/', userController.create);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
 
