1e3d827650f9f8a7dd3f88fd79ca3406a9569894
Ryan Lucas <ryanlcampos19@gmail.com>
2025-12-12 20:41:11 -0300

    :sparkles: Feat: adiciona aula (Eslint e Editorconfig)

--- REACT/listatarefas/src/App.js
+++ REACT/listatarefas/src/App.js
@@ -1,9 +1,7 @@
-import React from "react"; // Sempre importar
+import React from 'react'; // Sempre importar
 import './App.css';
 
 // Componente sem estado
 export default function App() {
-  return <h1>Hello World!</h1>
+  return <h1>Hello World!</h1>;
 }
-
-
