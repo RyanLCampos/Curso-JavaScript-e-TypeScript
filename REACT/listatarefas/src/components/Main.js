ea4d479599d29690a2fee37b1c5eef373b094b51
Ryan Lucas <ryanlcampos19@gmail.com>
2025-12-14 13:10:11 -0300

    :sparkles: Feat: adiciona aula (Estilizando Form e Tarefas)

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,30 +1,58 @@
 import React, { Component } from 'react';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer Café',
+      'Estudar',
+      'Fazer Compras'
+    ]
   };
 
   handleChange = (e) => {
     this.setState({
-      novaTarefa: e.target.value,
+      novaTarefa: e.target.value
     });
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas} = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
-        <p>{novaTarefa}</p>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className='edit' />
+                <FaWindowClose className='delete' />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
