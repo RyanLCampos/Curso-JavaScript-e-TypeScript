39c4fac0b358b8e099d3a080ce7ad2bf9b9f1c87
Ryan Lucas <ryanlcampos19@gmail.com>
2026-01-02 12:57:46 -0300

    :sparkles: Feat: adiciona aula (Router e History)

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,21 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/header';
 
 import Routes from './routes';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
