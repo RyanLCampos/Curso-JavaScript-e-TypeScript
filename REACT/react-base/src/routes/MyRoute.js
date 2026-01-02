4b23c4d60d04b0207cc960b584b40e010b17e999
Ryan Lucas <ryanlcampos19@gmail.com>
2026-01-02 13:13:43 -0300

    :sparkles: Feat: adiciona aula (MyRoute - Rota personalizada e privada)

---
+++ REACT/react-base/src/routes/MyRoute.js
@@ -0,0 +1,29 @@
+/* eslint-disable-next-line no-unused-vars */
+import React from 'react';
+
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{ pathname: '/login', state: { prevPath: rest.location.pathname } }}
+      />
+    );
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+MyRoute.defaultProps = {
+  isClosed: false, // Se não for enviado, será definido como False
+};
+
+MyRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]) // Elemento do React ou Função
+    .isRequired,
+  isClosed: PropTypes.bool, // É um boolean
+};
