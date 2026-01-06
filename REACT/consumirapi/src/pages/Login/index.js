df41bd45b27412e613b15c7a3d79bb75a99e8f68
Ryan Lucas <ryanlcampos19@gmail.com>
2026-01-06 14:59:35 -0300

    :sparkles: Feat: adiciona aula (Login de usuarios)

--- REACT/consumirapi/src/pages/Login/index.js
+++ REACT/consumirapi/src/pages/Login/index.js
@@ -1,12 +1,73 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
-export default function Login() {
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
+
+export default function Login(props) {
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    const senhaInvalido = password.length < 6 || password.length > 50;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido.');
+    }
+
+    if (senhaInvalido) {
+      formErrors = true;
+      toast.error('A senha deve ter entre 6 e 50 caracteres.');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password, prevPath }));
+  }
+
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@gmail.com"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua senha"
+          />
+        </label>
+        <div>
+          <button type="submit">Entrar</button>
+        </div>
+      </Form>
     </Container>
   );
 }
--- REACT/consumirapi/src/pages/Login/styled.js
+++ REACT/consumirapi/src/pages/Login/styled.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  input {
+    border: 1px solid #888888ff;
+    height: 40px;
+    padding-left: 10px;
+    border-radius: 4px;
+    margin-top: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  button {
+    margin-top: 20px;
+    width: 70%;
+  }
+`;
